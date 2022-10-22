cced6663fa95f3c91ec59891764a74496766438e
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-10-22 09:06:46 -0500

    Projects

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+	return (
+		<>
+		
+		<h1 className="text-primary text-4xl font-bold">
+			Hello world! I am using React
+		</h1>
+		<img src='https://www.tooltyp.com/wp-content/uploads/2014/10/1900x920-8-beneficios-de-usar-imagenes-en-nuestros-sitios-web.jpg' alt=''/>
+		</>
+	);
+}
+
+export default App;
